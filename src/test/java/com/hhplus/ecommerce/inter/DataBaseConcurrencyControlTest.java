2e82241e59297d54a119134a36932a0ef9cf461d
parrot <buj6842@naver.com>
2024-11-01 09:40:16 +0900

    Step 12 비관적 Lock 을 통한 시나리오 테스트 작성

--- src/test/java/com/hhplus/ecommerce/inter/DataBaseConcurrencyControlTest.java
+++ src/test/java/com/hhplus/ecommerce/inter/DataBaseConcurrencyControlTest.java
@@ -9,6 +9,7 @@ import com.hhplus.ecommerce.domain.user.User;
 import com.hhplus.ecommerce.infrastructure.OrderRepository;
 import com.hhplus.ecommerce.infrastructure.ProductRepository;
 import com.hhplus.ecommerce.infrastructure.UserRepository;
+import jakarta.persistence.OptimisticLockException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -20,6 +21,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -42,9 +44,15 @@ public class DataBaseConcurrencyControlTest {
     @Autowired
     private UserRepository userRepository;
 
+    @AfterEach
+    void tearDown() {
+        orderRepository.deleteAll();
+        productRepository.deleteAll();
+        userRepository.deleteAll();
+    }
     @Test
     @Transactional
-    void 동시성_테스트_DB_Lock() throws InterruptedException {
+    void 동시성_테스트_DB_Lock_유저_한명의_주문() throws InterruptedException {
         OrderRequest orderRequest = new OrderRequest(1L, List.of(new OrderItemDTO(1L, 1)), 5000);
 
         long startTime = System.currentTimeMillis();
@@ -78,5 +86,43 @@ public class DataBaseConcurrencyControlTest {
         // user 테이블 검증
         User updatedUser = userRepository.findById(1L).orElseThrow();
         assertEquals(499950000, updatedUser.getPoints(), "유저의 포인트 499999000");
+    }
+
+    @Test
+    @Transactional
+    void 동시성_테스트_여러_사용자가_같은상품에_대한_주문() throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        CountDownLatch latch = new CountDownLatch(10);
+
+        List<User> userList = userRepository.findAll();
+
+        for (int i = 0; i < 10; i++) {
+            User user = userList.get(i);
+            OrderRequest orderRequest = new OrderRequest(user.getUserId(), List.of(new OrderItemDTO(1L, 1)), 5000);
+
+            executorService.submit(() -> {
+                try {
+                    facade.orderProduct(orderRequest);
+                } catch (OptimisticLockException e) {
+                    System.out.println("Optimistic lock exception: " + e.getMessage());
+                } catch (Exception e) {
+                    System.out.println("에러발생" + e.getMessage());
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        executorService.shutdown();
+        latch.await(1, TimeUnit.MINUTES);
+
+        // order 테이블 검증
+        List<Order> orders = orderRepository.findAll();
+        assertEquals(10, orders.size(), "오더 개수 10");
+
+        // product 테이블 검증
+        Product product = productRepository.findByProductName("갤럭시s24");
+        assertEquals(490, product.getProductQuantity(), "상품 재고 작성");
+
     }
 }
